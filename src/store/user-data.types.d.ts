1447cd736ffe04960cf373ccac0d2a1dccbe7c9f
raylu <90059+raylu@users.noreply.github.com>
2026-04-08 23:16:30 -0700

    XIT ACT CX Sell

--- src/store/user-data.types.d.ts
+++ src/store/user-data.types.d.ts
@@ -69,7 +69,7 @@ declare namespace UserData {
     consumablesOnly?: boolean;
   }
 
-  type ActionType = 'CX Buy' | 'MTRA' | 'Refuel' | 'CONT Ship' | 'CONT Trade';
+  type ActionType = 'CX Buy' | 'CX Sell' | 'MTRA' | 'Refuel' | 'CONT Ship' | 'CONT Trade';
 
   interface ActionData {
     type: ActionType;
@@ -79,6 +79,7 @@ declare namespace UserData {
 
     allowUnfilled?: boolean;
     buyPartial?: boolean;
+    sellPartial?: boolean;
     exchange?: string;
     useCXInv?: boolean;
     priceLimits?: Record<string, number>;
