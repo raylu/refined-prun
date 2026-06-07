a1bc5cee7c8d2d55b2bf1d60ef811766ac4ca45e
raylu <90059+raylu@users.noreply.github.com>
2026-06-06 21:47:34 -0700

    comment out deserialize-prun-app

--- src/infrastructure/shell/index.ts
+++ src/infrastructure/shell/index.ts
@@ -2,7 +2,7 @@
 // if some other script in rprun fails to load. The code in this place also ensures
 // basic rprun functioning and update handling.
 
-import '@src/infrastructure/shell/deserialize-prun-app';
+//import '@src/infrastructure/shell/deserialize-prun-app';
 import '@src/infrastructure/shell/config';
 import '@src/infrastructure/shell/request-hooks';
 import '@src/infrastructure/shell/extension-update';
